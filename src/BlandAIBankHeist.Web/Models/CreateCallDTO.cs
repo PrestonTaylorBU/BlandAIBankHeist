c06d3617411a5c5b91d3af5b4bddfe94335e4f03
PrestonTaylorBU <s5733872@bournemouth.ac.uk>
2025-03-04 22:34:24 +0000

    Create an action that will queue a call for the game
    This commit is for adding the page and action for submitting a call to
    be queue. Currently it is non-functional and the calls to the AI's API
    will be made in a future commit.

---
+++ src/BlandAIBankHeist.Web/Models/CreateCallDTO.cs
@@ -0,0 +1,5 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlandAIBankHeist.Web.Models;
+
+public sealed record CreateCallDTO([Required, Phone(ErrorMessage = "The given phone number is invalid."), Display(Name = "Phone Number")] string PhoneNumberToCall);
